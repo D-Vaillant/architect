427502e6ae590053873ee97d1a03c2f1397228d4
D-Vaillant <dvaillant@gmail.com>
2015-05-16 14:22:04 -0700

    Starting work on new action system, inventory complexity.

---
+++ actions.py
@@ -0,0 +1,10 @@
+class Action:
+    def __init__(self, act_dict):
+        self.dict = None; # Figure this shit out.
+        self.proposition = act_dict["prop"]
+        
+    def call_action(thing1, thing2):
+        # Figure this shit out too.
+        return
+        
+    
